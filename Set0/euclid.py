0b24f2ec3c9dcced9ccf1bf8631aee6ca218adc4
Luiz Felipe Rosa da Silveira <luizfelipers19@gmail.com>
2021-02-02 14:56:19 -0300

    First set of exercises
    Euclidean distance between 2 points; Euclidean distance between 1 point and a line

---
+++ Set0/euclid.py
@@ -0,0 +1,8 @@
+x1=1
+y1=2
+x2=3
+y2=4
+
+euclidistancia = ((x2-x1)**2 + (y2-y1)**2)**0.5
+out = euclidistancia
+print(out)
---
+++ Set0/pontoReta.py
@@ -0,0 +1,9 @@
+px=1
+py=2
+a=3
+b=4
+c=5
+
+d = abs(a * px + b * py + c) / (px**2 + py**2)**0.5
+out = d
+print(out)
