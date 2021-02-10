c2a34b8f84e08bdddb777e874e114fdc30e07b79
Luiz Felipe Rosa da Silveira <luizfelipers19@gmail.com>
2021-02-09 23:13:58 -0300

    Update p3_3.py

--- Set3/p3_3.py
+++ Set3/p3_3.py
@@ -1,7 +1,8 @@
 #Mapeamento de dicionários
 #função que mapeia todos os valores dos elementos do dicionário passado como argumento, e para cada elemento, executa a função também passada por argumento
-
 def dictmap(d, f):
-    for value in d:
-        d[value] = f(d[value])
-    return None
+    #percorre o dicionário d para substituir os valores
+    for valor in d:
+      #Os valores do dicionário são atualizados com o resultado da função definida para cada valor. A função é definida pelo Gradescope
+        d[valor] = f(d[valor])
+    #sem return, pois a função não deve possuir retorno
