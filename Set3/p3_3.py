eb33323d7185ad0e39c6161b275022e21002e652
Luiz Felipe Rosa da Silveira <luizfelipers19@gmail.com>
2021-02-09 23:13:22 -0300

    Update p3_3.py

--- Set3/p3_3.py
+++ Set3/p3_3.py
@@ -1,3 +1,6 @@
+#Mapeamento de dicionários
+#função que mapeia todos os valores dos elementos do dicionário passado como argumento, e para cada elemento, executa a função também passada por argumento
+
 def dictmap(d, f):
     for value in d:
         d[value] = f(d[value])
