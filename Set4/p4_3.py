2e3cd86c67d1366d96ff394d5fbf2337239cad95
Luiz Felipe Rosa da Silveira <luizfelipers19@gmail.com>
2021-02-11 22:39:39 -0300

    calculo do MDC e Redução para forma mínima da fração

--- Set4/p4_3.py
+++ Set4/p4_3.py
@@ -4,9 +4,13 @@ class Rational:
         self.denominador = denominador
 
     def maximoDivComum(self):
+        #Algoritimo Euclideano
         nume = self.numerador
         denomi = self.denominador
-        while denomi == True:
+        #enquanto o denominador for maior que o numerador
+        while denomi:
+            #inverte numerador e denominador, e retorna para o numerador,
+            # o resto da divisão entre numerador e denominador
             nume, denomi = denomi, nume % denomi
         return nume
     
@@ -14,11 +18,13 @@ class Rational:
     def get_numerator(self):
         valor = self.numerador
         int(valor)
+        print(valor)
         return valor    
 
     def get_denominator(self):
         valor = self.denominador
         int(valor)
+        print(valor)
         return valor
     
     def to_float(self):
@@ -27,7 +33,23 @@ class Rational:
         numera = self.get_numerator()
         denomina = self.get_denominator()
         razao = float(numera / denomina)
+        print(razao)
         return razao
     
     
-        
+    #Retorna o reciproco, a partir da dúvida tirada em aula do dia 11/02
+    #Reciproco é o inverso
+    def reciprocal(self):
+        #Para uma fração, o número recíproco é somente uma fração diferente,
+        # com os números “trocados”, o de cima pelo de baixo. 
+        # Por exemplo, o recíproco de 3/4 é 4/3.
+        print(Rational(self.get_denominator(), self.get_numerator()))
+        return Rational(self.get_denominator(), self.get_numerator())
+    
+    def reduce(self):
+        #Reduz a fração o máximo possível, a partir do máximo divisor comum encontrado
+        mdc = self.maximoDivComum()
+        #retorna um numero de classe Rational, a partir da divisão do numerador e denominador pelo MDC encontrado
+        reduzidaAoMaximo = Rational(int(self.numerador / mdc), int(self.denominador / mdc))
+        print(reduzidaAoMaximo)
+        return reduzidaAoMaximo
