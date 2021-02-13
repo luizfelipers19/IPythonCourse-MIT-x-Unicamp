bbf685d0f8fac82de57c458d3e95e64576ac90ae
Luiz Felipe Rosa da Silveira <luizfelipers19@gmail.com>
2021-02-12 23:20:19 -0300

    Matrices basic methods (init constructor, size and returning itself)

--- Set4/p4_4.py
+++ Set4/p4_4.py
@@ -1,1 +1,13 @@
 #Operação em vetores
+import math
+
+class Vector:
+    
+    def __init__(self, lista):
+        self.lista = lista
+        
+    def as_list(self):
+        return self.lista
+    
+    def size(self):
+        return len(self.lista)
