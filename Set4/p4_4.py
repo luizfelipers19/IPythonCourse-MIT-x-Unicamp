ad4d5a1833c8b8c4536ab06f951ce2f56ffb064c
Luiz Felipe Rosa da Silveira <luizfelipers19@gmail.com>
2021-02-12 23:21:07 -0300

    Calculates the magnitude of a vector, and some maths operations in a vector

--- Set4/p4_4.py
+++ Set4/p4_4.py
@@ -11,3 +11,36 @@ class Vector:
     
     def size(self):
         return len(self.lista)
+#Magnitude do vetor (fórmula achada na net)
+    def magnitude(self):
+        totalMag = 0
+        for counter in self.lista:
+            totalMag += counter * counter
+      
+        return totalMag ** (.5)
+    
+    
+    #CALCULA A
+    #distancia euclidiana entre os elementos do vetor    
+    def euclidean_distance(self, other):
+        totalDist = 0
+        for counter in range(len(self.lista)):
+            totalDist = totalDist + (self.lista[counter] - other.as_list()[counter]) ** 2
+        return totalDist ** (.5)
+ 
+    #soma dois vetores muito doidos
+    def __add__(self, other):
+        #caso o outro argumento passado não seja um vetor,
+        if isinstance(other, Vector) == False:
+            return None
+        #caso seja vetor, cria um novo vetor e percorre elemento por elemento, somando
+        elif isinstance(other, Vector) == True:
+            if len(self.lista) == len(other.lista):
+                novoVetor = Vector(self.lista)
+                for itens in range(len(other.lista)):
+                    novoVetor.lista[itens] = self.lista[itens] + other.lista[itens]
+                return novoVetor
+        
+        else:
+            return None
+        
