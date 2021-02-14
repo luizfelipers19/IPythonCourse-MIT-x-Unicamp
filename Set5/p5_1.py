68e816f3667d108c36bd2b1831d9dbd55c3c24b6
Luiz Felipe Rosa da Silveira <luizfelipers19@gmail.com>
2021-02-13 22:33:19 -0300

    Update p5_1.py

--- Set5/p5_1.py
+++ Set5/p5_1.py
@@ -20,43 +20,42 @@ def run_langton(rules, size):
     #oeste = 180
     #sul = 270
     #leste = 0 / 360
-    
-    
-    
+
     #inicialização do contador de passos da formiguinha do capeta
     marcaPasso = 0
     valorColorido = []  #guarda as cores, que não ão apenas 'preto' e 'branco'
     tabuleiro = []  
-    
+
     #cria o tabuleiro a partir do tamanho passado como argumento na função principal
     def gera_tabuleiro():
         #percorre todas as linhas e
         for linha in range(size):
             #adiciona um espaço vazio 
             valorColorido.append([])
-            #agora acessa cada elemento da matriz e os inicializa com cor 0 
+            #agora acessa cada elemento da matriz e os inicializa com cor 0
             for coluna in range(size):
                 valorColorido[linha].append(0)
                 tabuleiro.append((coluna, linha))
-                
+
     def atualiza_cor():
         #Segundo o enunciado, a cada passagem numa casa,
         # soma-se 1 ao valor da cor desse elemento
         # pode ser qualquer valor
-        valorColorido[formiX][formiY] = valorColorido[formiX][formiY] + 1 #soma 1 ao valor atual da casa da formiga
-        
-        
+        valorColorido[formiY][formiX] = valorColorido[formiY][formiX] + 1 #soma 1 ao valor atual da casa da formiga
         #Lógica do Matheus castello no Piazza
-        valorColorido[formiX][formiY] = valorColorido[formiX][formiY] % len(rules) #
+        valorColorido[formiY][formiX] =valorColorido[formiY][formiX] % len(rules)
     
     
-    #cria um tabuleiro a partir do Size passado no início    
+    #cria um tabuleiro a partir do Size passado no início 
     gera_tabuleiro()
     #atualiza a cor dos elementos
     atualiza_cor()
     #o ponto inicial precisa ser contado como um passo, então marca passo é incrementado
-    marcaPasso = marcaPasso +1
     
+    
+    #inicializado com um passo extra, uma vez que o teste
+    # pede o passo em que a formiga saiu do tabuleiro
+    marcaPasso = marcaPasso +1
     formiY = formiY -1
     
     #### aaaaaaaaaaaaaah não sei o que fazer agora pqpqp
@@ -70,47 +69,54 @@ def run_langton(rules, size):
     elif size == 0:
         return(0, [])
     
-    #itera em todos os elementos da string Rules passada, para 
-    letra = 0
+    ### O ERRO ESTAVA AQUI!! ANTES ESTAVA FAZENDO UMA ITERAÇÃO ENTRE A STRING DE REGRAS
+    #E DEVERIA ESTAR CHECANDO AS CONDIÇ~ES EXPRESSAS NO ENUNCIADO
     
-    for letra in range(0, len(rules)):
-        if rules[letra] == 'L':
-            angulo = angulo - 90
-            
+    # CORRIGIDO COM AJUDA DE PEDRO HENRIQUE LIMA, VICTOR E CARLOS. Obrigado amigos, vocês são amigos.
+    
+    #LOOP PRINCIPAL
+    #Enquanto a posição da formiga ainda estiver contida no tabuleiro
+    while (size - 1) >= formiX >= 0 and (size - 1) >= formiY >= 0:
+         
+        casaCor = valorColorido[formiY][formiX]
+        if rules[casaCor] == 'R':
+            angulo = angulo +90 
         
-        elif rules[letra] == 'R':
-            angulo = angulo + 90
+        if rules[casaCor] == 'L':
+            angulo = angulo - 90
+        
             
         #pega o valor da soma dos ângulos e normaliza, ao dividir por 360
         # e pegar o resto da divisão, para saber qual o ângulo atual
-        anguloNormalizado = angulo % 360
+        anguloNormalizado = angulo %360
         
-        #chama a função de atualizar a cor para somar +1 ao valor da casa atual
-        atualiza_cor()
-    
-        #Computa o próx passo da formiga, ou seja, 
+        #chama a função de atualizar a cor para somar +1 ao valor da casa atual	
+        atualiza_cor()	
+    	
+        #Computa o próx passo da formiga, ou seja, 	
         # atualiza o valor da posição X ou Y, dependendo do ânguloNormalizado retornado
         
         #Incrementa o eixoX anda um para a direita se o ângulo apontar para a direita
         if anguloNormalizado == 0:
+            #formiX += 1
             formiX = formiX + 1
-        #decrementa o eixoY se o angulo apontar para o Norte
+            
+        #decrementa o eixoY se o angulo apontar para o Norte    
         if anguloNormalizado == 90:
+            #formiY -= 1
             formiY = formiY -1
         #decrementa o eixo X caso esteja apontando para Leste atual
         if anguloNormalizado == 180:
+            #formiX -= 1
             formiX = formiX -1
         #incrementa o eixo Y
         if anguloNormalizado == 270:
+            #formiY += 1
             formiY = formiY +1
-            
         #incrementa o contador de passos da fomriga
         marcaPasso = marcaPasso +1
-    
-    print(marcaPasso, valorColorido)
-    return(marcaPasso, valorColorido)
-        
+    print(marcaPasso, valorColorido)     
+    return (marcaPasso, valorColorido)
+
 #caso de teste para ver se a formiga soma o valor acima de 1       
-print(run_langton('LLRLLLLLLRLRLRR', 15))
-    
-    
+print(run_langton('RL', 50))
